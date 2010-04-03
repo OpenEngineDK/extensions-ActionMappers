d72c9dcdd41c0f4754613b432ed3d44fc78db873
ptx <ptx@openengine.dk>
2010-04-03 14:57:03 +0000

    Added actioncallback

---
+++ Utils/ActionCallback.h
@@ -0,0 +1,35 @@
+// 
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS)
+//
+// This program is free software; It is covered by the GNU General
+// Public License version 2 or any later version.
+// See the GNU General Public License for more details (see LICENSE).
+//--------------------------------------------------------------------
+
+
+#ifndef _OE_ACTION_CALLBACK_H_
+#define _OE_ACTION_CALLBACK_H_
+
+
+namespace OpenEngine {
+namespace Utils {
+
+    template <class T, class A>
+    class ActionCallback : public IListener<A> {
+        T* callee;
+        void (T::*callback)(A);
+    public:
+        ActionCallback(T* who, void (T::*func)(A)) 
+            : callee(who),callback(func) {}
+        
+        void Handle(A arg) {
+            (callee->*callback)(arg);
+        }
+    };
+
+
+} // NS Utils
+} // NS OpenEngine
+
+#endif // _OE_ACTION_CALLBACK_H_
